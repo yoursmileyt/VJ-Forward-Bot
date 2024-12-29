f1a62a2cb5802570e335ee5c29b9d82e73ec619c
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-29 11:25:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
 
 #==================Restart Function==================#
 
-@Client.on_message(filters.private & filters.command(['restart']) & filters.user(1957296068))
+@Client.on_message(filters.private & filters.command(['restart']) & filters.user(Config.BOT_OWNER))
 async def restart(client, message):
     msg = await message.reply_text(text="<i>Trying to restarting.....</i>")
     await asyncio.sleep(5)
