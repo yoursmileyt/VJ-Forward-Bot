057652791443e1c32e79ffd21ce619723913561d
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-29 11:36:23 +0530

    Update db.py

--- plugins/db.py
+++ plugins/db.py
@@ -1,5 +1,13 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import asyncio
 import motor.motor_asyncio
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 class MongoDB:
     def __init__(self, uri, db_name, collection):
@@ -33,7 +41,10 @@ class MongoDB:
     async def drop_all(self):
         return await self.files.drop()
         
-# Example usage:
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 async def connect_user_db(user_id, uri, chat):
     chat = f"{user_id}{chat}"
     dbname = f"{user_id}-Forward-Bot"
@@ -45,4 +56,6 @@ async def connect_user_db(user_id, uri, chat):
         return False, db
     return True, db
     
-    
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
