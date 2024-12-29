5e54283fcaf49cb02122fa3ccadd4d5843393634
Tech VJ <122880036+VJBots@users.noreply.github.com>
2024-12-29 12:09:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 import asyncio, logging
 from config import Config
 from pyrogram import Client as VJ, idle
@@ -5,6 +9,9 @@ from typing import Union, Optional, AsyncGenerator
 from logging.handlers import RotatingFileHandler
 from plugins.regix import restart_forwards
 
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
 
 if __name__ == "__main__":
     VJBot = VJ(
@@ -62,3 +69,7 @@ if __name__ == "__main__":
         await idle()
 
     asyncio.get_event_loop().run_until_complete(main())
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
